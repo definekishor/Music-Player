75b2a34a9b46e42285f185e004a326de8e7e32d5
definekishor <definekishor@gmail.com>
2017-07-08 12:17:21 +0530

    Intial commit

---
+++ AudioPlayerApp/AppDelegate.swift
@@ -0,0 +1,35 @@
+//
+//  AppDelegate.swift
+//  LGAudioPlayerLockScreen
+//
+//  Created by Kishor on 22/05/17.
+//  Copyright © 2017 Inscript India Pvt Ltd. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import MediaPlayer
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    var player: AudioPlayer!
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        let audioSession = AVAudioSession.sharedInstance()
+        let commandCenter = MPRemoteCommandCenter.shared()
+        let nowPlayingInfoCenter = MPNowPlayingInfoCenter.default()
+        let notificationCenter = NotificationCenter.default
+        let bundle = Bundle.main
+        
+        self.player = AudioPlayer(dependencies: (audioSession, commandCenter, nowPlayingInfoCenter, notificationCenter))
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window!.makeKeyAndVisible()
+        self.window!.rootViewController = PlaylistViewController(dependencies: (self.player, bundle, notificationCenter))
+        
+        return true
+    }
+    
+}
---
+++ AudioPlayerApp/PlaybackItemCell.swift
@@ -0,0 +1,60 @@
+//
+//  PlaybackItemCell.swift
+//  LGAudioPlayerLockScreen
+//
+//  Created by Kishor on 22/05/17.
+//  Copyright © 2017 Inscript India Pvt Ltd. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PlaybackItemCell: UITableViewCell {
+
+    //MARK: - Vars
+    
+    let playbackItem: PlaybackItem
+    var barsImageView: UIImageView?
+    
+    //MARK: - Dependencies
+    
+    let player: AudioPlayer
+    
+    //MARK: - Init
+    
+    init(player: AudioPlayer, playbackItem: PlaybackItem) {
+        self.playbackItem = playbackItem
+        self.player = player
+
+        super.init(style: .subtitle, reuseIdentifier: nil)
+        
+        self.updateView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("NSCoding not supported")
+    }
+    
+    //MARK: - Update View
+    
+    func updateView() {
+        self.imageView?.image = UIImage(named: self.playbackItem.albumImageName)
+        self.textLabel?.text = "\(self.playbackItem.artistName) - \(self.playbackItem.trackName)"
+        self.detailTextLabel?.text = "\(self.playbackItem.albumName)"
+        
+       
+    }
+    
+   
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+    }
+    
+    override func setHighlighted(_ highlighted: Bool, animated: Bool) {
+        super.setHighlighted(highlighted, animated: animated)
+        
+    }
+    
+}
